3855c1262dc74a5ea763252729e0d142af3e473a
Tamirat Kebede <tamiratkebede120@gmail.com>
2024-04-10 21:58:12 +0300

    dummy questions is added

--- quiz/lib/questions_screen.dart
+++ quiz/lib/questions_screen.dart
@@ -1,15 +1,44 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class QuestionsScreen extends StatefulWidget{
+class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
   @override
   State<QuestionsScreen> createState() {
     return _QuestionsScreenState();
   }
 }
-class _QuestionsScreenState extends State<QuestionsScreen>{
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(child:  Text("Questions Screen"));
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("actual questiosn"),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("answer 1"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("answer 2"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("answer 3"),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text("answer 4"),
+          ),
+        ],
+      ),
+    );
   }
 }
