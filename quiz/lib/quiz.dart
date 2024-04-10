676ab6ca1ac5bdac0f3bc70a30ec2b0b5fe533f8
Tamirat Kebede <tamiratkebede120@gmail.com>
2024-04-10 21:30:39 +0300

    different approach to render 3 approach

--- quiz/lib/quiz.dart
+++ quiz/lib/quiz.dart
@@ -1,45 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/start_screen.dart';
 import 'package:quiz/questions_screen.dart';
-class Quiz extends StatefulWidget{
+
+class Quiz extends StatefulWidget {
   const Quiz({super.key});
   @override
   State<Quiz> createState() {
-    return  _QuizState();
+    return _QuizState();
   }
-
-
 }
 
-
-class _QuizState extends State<Quiz>{
+class _QuizState extends State<Quiz> {
   var activeScreen = "start_screen";
-  void switchScreen(){
+  void switchScreen() {
     setState(() {
       activeScreen = "questions_screen";
     });
   }
-  @override
 
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    var screenWidget = activeScreen == "questions_screen"
+        ? const QuestionsScreen()
+        : StartScreen(switchScreen);
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        
-        body:  Container(
+        body: Container(
           decoration: const BoxDecoration(
               gradient: LinearGradient(
             colors: [
               Color.fromARGB(255, 78, 12, 151),
               Color.fromARGB(255, 107, 15, 168),
-
             ],
             begin: Alignment.topLeft,
-            end:Alignment.bottomRight,
+            end: Alignment.bottomRight,
           )),
-          child: activeScreen =="questions_screen"?const QuestionsScreen(): StartScreen(switchScreen),
+          child: screenWidget,
         ),
       ),
     );
   }
-} 
+}
