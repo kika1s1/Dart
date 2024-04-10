7d55830dc02e6768b3c685086aca9bb4261d0d51
Tamirat Kebede <tamiratkebede120@gmail.com>
2024-04-10 21:20:34 +0300

    making different screen rendering conditionally

--- quiz/lib/main.dart
+++ quiz/lib/main.dart
@@ -1,32 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:quiz/start_screen.dart';
-
+import 'package:quiz/quiz.dart';
 void main() {
-  runApp(const Quiz());
+  runApp(const MyApp());
 }
 
-class Quiz extends StatelessWidget {
-  const Quiz({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        
-        body:  Container(
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 78, 12, 151),
-              Color.fromARGB(255, 107, 15, 168),
-
-            ],
-            begin: Alignment.topLeft,
-            end:Alignment.bottomRight,
-          )),
-          child: const StartScreen(),
-        ),
-      ),
-    );
+    return const Quiz();
   }
 }
