c393e647c2ce0f88fd483dc88d09e7f7765c56ba
Tamirat Kebede <tamiratkebede120@gmail.com>
2024-04-10 23:31:12 +0300

    shuffling questions

--- quiz/lib/models/quiz_question.dart
+++ quiz/lib/models/quiz_question.dart
@@ -2,5 +2,10 @@ class QuizQuestion {
   const QuizQuestion(this.text, this.answers);
   final String text;
   final List<String> answers;
+  List<String> getShuffledAnswers(){
+    final shuffledAnswer = List.of(answers);
+    shuffledAnswer.shuffle();
+    return shuffledAnswer;
+  }
 
 }
